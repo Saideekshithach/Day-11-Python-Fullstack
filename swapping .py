9a4baa9bf6b5d280c094cbbc392b67abe6b314cf
Saideekshitha <104785671+Saideekshithach@users.noreply.github.com>
2024-11-04 19:46:36 +0530

    Add files via upload

---
+++ swapping .py
@@ -0,0 +1,82 @@
+"""a=int(input())
+b=int(input())
+print(a+b)"""
+"""a=10
+b=20
+temp=a
+a=b
+b=temp
+print(a)
+print(b)"""
+"""a=10
+b=20
+a,b=b,a
+print("a value is",a)
+print)"b value is",b)
+"""
+"""a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping",a)
+print("after swappung",b)"""
+
+"""a=int(input())
+b=int(input())
+a,b=b,a
+print("a value is",a)
+print("b value is",b)
+
+a=int(input())
+b=int(input())
+temp=a
+a=b
+b=temp
+print(a)
+print(b)"""
+
+"""a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d"%(a,b))
+
+a=10.7
+b=20.8
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%f,b=%f"%(a,b))"""
+
+
+"""a=input("data1")
+b=input("data2")
+temp=a
+a=b
+b=temp
+print("after swapp ing a=%s b=%s"%(a,b))"""
+
+"""a=10
+b=20
+print("sum is {}".format(a+b))"""
+
+'''a=input()
+b=input()
+print(f"strings are {a} {b} ")'''
+
+
+"""a=15
+b=20
+print("sum of {} {} is {} ".format(a,b,a+b))"""
+
+a=int(input())
+b=int(input())
+c=a+b
+print(f"sum is {c}")
+print(f"the sum is",{c})
+
+
+
+
